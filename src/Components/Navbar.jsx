fce4ba4431d0a1799ba13beaf52b3963480c8769
Alfred Francis <alfredfrancis@Alfreds-MacBook-Air.local>
2025-07-11 12:02:29 +0530

    Second commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
 <nav class="navbar navbar-expand-lg bg-secondary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Product App</a>
+    <Link class="navbar-brand" href="/">Product App</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
